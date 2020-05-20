039cfcb1df0e02f62c94b0919bb023b1709af0fc
Olga Bugrova <Lelkabygr@mail.ru>
2020-05-20 10:28:09 +0300

    feat: add handle func from class TodoItem

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 export class TodoItem extends Component {
-  onHandleClick = () => {
-    console.log(`${this.props.todo}`);
+  state = {
+    done: false,
+    important: false,
+  };
+
+  onHandleClickItem = () => {
+    this.setState({
+      done: true,
+    });
+  };
+
+  onHandleClickImportant = () => {
+    this.setState({
+      important: true,
+    });
   };
 
   render() {
-    const style = {
-      color: this.props.important ? 'tomato' : 'black',
-      fontWeight: this.props.important ? 'bold' : 'normal',
-    };
+    const { done, important } = this.state;
+
+    let todoListItem = 'todo-list-item d-flex';
+    if (done) {
+      todoListItem += ' done';
+    }
+    if (important) {
+      todoListItem += ' important';
+    }
+
     return (
-      <span className="todo-list-item d-flex">
-        <span style={style} className="todo-list-item-name" onClick={this.onHandleClick}>
+      <span className={todoListItem}>
+        <span className="todo-list-item-name" onClick={this.onHandleClickItem}>
           {this.props.todo}
         </span>
         <span className="item-button">
-          <button type="button" className="btn btn-outline-success btn-sm">
+          <button type="button" className="btn btn-outline-success btn-sm" onClick={this.onHandleClickImportant}>
             <i className="fa fa-exclamation" />
           </button>
           <button type="button" className="btn btn-outline-danger btn-sm">
