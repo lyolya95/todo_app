d2cc6ce5100dd707e3abe5addf36a9c47f16cf6c
Olga Bugrova <Lelkabygr@mail.ru>
2020-05-22 09:41:25 +0300

    feat: add submit form

--- src/components/PanelAddItem/PanelAddItem.jsx
+++ src/components/PanelAddItem/PanelAddItem.jsx
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import './PanelAddItem.css';
 
 export class PanelAddItem extends Component {
+  state = {
+    label: '',
+  };
+
+  onValueChange = (event) => {
+    this.setState({
+      label: event.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddItem(this.state.label);
+    this.setState({
+      label: '',
+    });
+  };
+
   render() {
-    const { onAddItem } = this.props;
     return (
-      <form className="panel-add-item d-flex">
-        <input type="text" placeholder="Enter todo..." className="form-control" onChange={this.onValueChange} />
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          onClick={() => {
-            onAddItem('ggggggg');
-          }}
-        >
-          Add
-        </button>
+      <form className="panel-add-item d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="Enter todo..."
+          className="form-control"
+          onChange={this.onValueChange}
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary">Add</button>
       </form>
     );
   }
